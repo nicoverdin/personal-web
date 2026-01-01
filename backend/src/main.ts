26a4525eb6e789845aab8f0130660ba5ef3fa10d
nicoverdin <nverdind@gmail.com>
2026-01-01 22:05:49 +0100

    fix: add production domain to cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://nicolasverdin.com',
+      'https://www.nicolasverdin.com',
+    ],
   });
 
   app.useGlobalPipes(
